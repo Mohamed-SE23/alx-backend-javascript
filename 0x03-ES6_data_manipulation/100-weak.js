6e8687cae69a864ae6f201084e9e9d407d5768c9
MohamedAlamen123 <alamenmohamed536@gmail.com>
2024-10-08 15:18:27 +0200

    Weak link data structure

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+export const weakMap = new WeakMap();
+
+export const queryAPI = (endpoint) => {
+    // check if the endpoint exist in weakMap
+    const queryCount = weakMap.get(endpoint) || 0;
+
+    // check if the number of queries is greater than or equal 5 times
+    if (queryCount >= 4) { // start from 0 to 4
+        throw new Error ('Endpoint load is high')
+    }
+
+    // increment query count and update the weakMap
+    weakMap.set(endpoint, queryCount + 1);
+}
