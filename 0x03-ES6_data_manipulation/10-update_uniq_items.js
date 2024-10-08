2c37f3698fca3f144fd48bcee0a218fe5120890d
MohamedAlamen123 <alamenmohamed536@gmail.com>
2024-10-08 11:50:24 +0200

    more map data structure

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,17 @@
+const updateUniqueItems = (map) => {
+    // Check if the argument is a Map
+    if (!(map instanceof Map)) {
+        throw new Error('Cannot process');
+    }
+
+    // Iterate through the map entries
+    for (const [key, value] of map) {
+        if (value === 1) {
+            map.set(key, 100);  // Update quantity to 100 if it's 1
+        }
+    }
+
+    return map;  // Return the updated map
+};
+
+export default updateUniqueItems;
