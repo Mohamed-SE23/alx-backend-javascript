5e71f3b9acb029abc1baa2438ebb6b11cc5ea6ac
MohamedAlamen123 <alamenmohamed536@gmail.com>
2024-10-08 10:19:55 +0200

    add function hasValuesFromArray

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,14 @@
+const hasValuesFromArray = (set, arr) => {
+    let result = false;
+    for (const elem of arr){
+        if (set.has(elem)){
+            result = true;
+        } else {
+            result = false;
+        }
+        
+    }
+    return result;
+};
+
+export default hasValuesFromArray;
